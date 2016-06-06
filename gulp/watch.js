276645af3b942dd9e13d2f9c768c24dcdad03f57
paolo <uypaodg@gmail.com>
2016-06-06 15:54:01 +0800

    first commit

---
+++ gulp/watch.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var path = require('path');
+var gulp = require('gulp');
+var conf = require('./conf');
+
+var browserSync = require('browser-sync');
+
+function isOnlyChange(event) {
+  return event.type === 'changed';
+}
+
+gulp.task('watch', ['inject'], function () {
+
+  gulp.watch([path.join(conf.paths.src, '/*.html'), 'bower.json'], ['inject-reload']);
+
+  gulp.watch(path.join(conf.paths.src, '/css/**/*.css'), function(event) {
+    if(isOnlyChange(event)) {
+      browserSync.reload(event.path);
+    } else {
+      gulp.start('inject-reload');
+    }
+  });
+
+  gulp.watch(path.join(conf.paths.src, '/js/**/*.js'), function(event) {
+    if(isOnlyChange(event)) {
+      gulp.start('scripts-reload');
+    } else {
+      gulp.start('inject-reload');
+    }
+  });
+
+});
